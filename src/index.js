5ecd52d3def2ee2836e95c34093ca8c5014ef685
Your Name <you@example.com>
2026-01-04 17:44:09 +0100

    Comment update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 // src/index.js
-import "./styles/styles.css"
-import { loadPage } from "./page-loader.js"
-import { createHomePage } from "./pages/home.js"
-import { createMenuPage } from "./pages/menu.js"
-import { ContactTab } from "./pages/contact.js"
+import "./styles/styles.css";
+import { loadPage } from "./page-loader.js";
+import { createHomePage } from "./pages/home.js";
+import { createMenuPage } from "./pages/menu.js";
+import { ContactTab } from "./pages/contact.js";
 
-const buttons = document.querySelectorAll('button')
-loadPage((createHomePage()))
-buttons.forEach(button => button.addEventListener('click', function (e) {
-    e.preventDefault()
-    if (e.target.dataset['tab'] === "home") {
-        loadPage(createHomePage())
-    } else if (e.target.dataset['tab'] === "menu") {
-        loadPage(createMenuPage())
+const buttons = document.querySelectorAll("button");
+loadPage(createHomePage());
+buttons.forEach((button) =>
+  button.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (e.target.dataset["tab"] === "home") {
+      loadPage(createHomePage());
+    } else if (e.target.dataset["tab"] === "menu") {
+      loadPage(createMenuPage());
     } else {
-        loadPage(new ContactTab().element)
+      loadPage(new ContactTab().element);
     }
-}))
+  })
+);
